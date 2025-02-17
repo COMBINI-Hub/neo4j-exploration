78444d75411c4c9c19600e7418ebfe80f5caa3da
Drshika Asher <67125579+drshika@users.noreply.github.com>
2025-02-17 02:07:23 -0500

    wip conversion

--- t.py
+++ t.py
@@ -1,6 +1,6 @@
 import gzip
 
-with gzip.open('data/entity.gz', 'rt') as f:  # 'rt' mode for text reading
+with gzip.open('semmed_data/entity.csv.gz', 'rt') as f:  # 'rt' mode for text reading
     for i, line in enumerate(f):
         if i >= 5:  # Print first 5 lines
             break
